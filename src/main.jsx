de28b65d207e600d79ce5838ef0a6fce6e2d5e76
Shamiul40 <shamiul4040@gmail.com>
2025-08-24 00:48:07 +0600

    authContext set up

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import { RouterProvider } from 'react-router'
 import { router } from './router/router.jsx'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import AuthProvider from './Context/AuthProvider.jsx'
 
 AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+   <AuthProvider>
+     <RouterProvider router={router} />
+   </AuthProvider>
   </StrictMode>,
 )
